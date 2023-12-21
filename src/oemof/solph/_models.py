1872962e0c9232bcb2c2df9c0910a27e48401836
KarolinaBartoszuk <68282414+KarolinaBartoszuk@users.noreply.github.com>
2023-12-21 11:00:45 +0100

    test for pip installation of oemoflex

--- src/oemof/solph/_models.py
+++ src/oemof/solph/_models.py
@@ -280,6 +280,8 @@ class BaseModel(po.ConcreteModel):
         self.es.results = solver_results
         self.solver_results = solver_results
 
+        logging.info("##################################TESTING SUCCESSFUL#####################")
+
         return solver_results
 
     def relax_problem(self):
